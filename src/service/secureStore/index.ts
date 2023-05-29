a69d72f0305840e69f791f720e6fd5a42bf6f5c8
Siim <siim.kirjanen@gmail.com>
2023-05-29 12:21:26 +0300

    remove storage method, renaming

--- src/service/secureStore/index.ts
+++ src/service/secureStore/index.ts
@@ -7,9 +7,9 @@ import { IToken } from '../../interface';
 export async function getStoredApiTokenData() {
   try {
     const storedApiTokenData = [];
-    const tokenIds = await getStoredApiTokenIds();
+    const tokenIdsSet = await getStoredApiTokenIds();
 
-    for (const tokenId of tokenIds) {
+    for (const tokenId of tokenIdsSet) {
       const result = await SecureStore.getItemAsync(`${SECURE_STORE_KEY}_${tokenId}`);
       if (result) {
         storedApiTokenData.push(JSON.parse(result));
@@ -22,16 +22,10 @@ export async function getStoredApiTokenData() {
   }
 }
 
-export async function getStoredApiTokenIds() {
+async function getStoredApiTokenIds() {
   const result = await AsyncStorage.getItem(STORED_API_TOKEN_IDS);
 
   return result ? new Set(JSON.parse(result)) : new Set();
-}
-
-export async function storeApiTokenIds(tokenIds) {
-  const value = JSON.stringify(Array.from(tokenIds));
-
-  await AsyncStorage.setItem(STORED_API_TOKEN_IDS, value);
 }
 
 export async function storeApiTokenData(tokenData: IToken) {
@@ -49,6 +43,21 @@ export async function storeApiTokenData(tokenData: IToken) {
   }
 }
 
-export async function removeStoredApiIds() {
-  await AsyncStorage.removeItem(STORED_API_TOKEN_IDS);
+async function storeApiTokenIds(tokenIds) {
+  const value = JSON.stringify(Array.from(tokenIds));
+
+  await AsyncStorage.setItem(STORED_API_TOKEN_IDS, value);
+}
+
+export async function clearAllStorage() {
+  try {
+    const tokenIdsSet = await getStoredApiTokenIds();
+
+    for (const tokenId of tokenIdsSet) {
+      await SecureStore.deleteItemAsync(`${SECURE_STORE_KEY}_${tokenId}`);
+    }
+    await AsyncStorage.removeItem(STORED_API_TOKEN_IDS);
+  } catch (e) {
+    alert(e.message);
+  }
 }
