a69c114f40ee9aed77a7e201cabcfe8961ff2afe
Siim <siim.kirjanen@gmail.com>
2023-06-28 19:01:21 +0300

    system alerts scrollable

--- src/components/NotificationBell/index.tsx
+++ src/components/NotificationBell/index.tsx
@@ -1,6 +1,6 @@
 import { Dialog, Icon } from '@rneui/themed';
 import React, { useContext, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 import { AppContext } from '../../context/AppContext';
 
@@ -17,9 +17,9 @@ export function NotificationBell() {
       <Dialog
         isVisible={notificationsVisible}
         onBackdropPress={() => setNotificationsVisible(false)}>
-        <Dialog.Title title="Notifications" />
-        {globalConfig &&
-          globalConfig.alerts.map((alert, i) => {
+        <Dialog.Title title="System alerts" />
+        <ScrollView>
+          {globalConfig.alerts.map((alert, i) => {
             return (
               <Text key={i}>
                 <Text style={{ fontWeight: 'bold' }}>
@@ -33,6 +33,7 @@ export function NotificationBell() {
               </Text>
             );
           })}
+        </ScrollView>
       </Dialog>
     </View>
   );
